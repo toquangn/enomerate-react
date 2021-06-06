d233ffe99e8a1055f9359195f169c241d7cd052a
unknown <dev.to.quang.n@gmail.com>
2021-06-06 16:05:56 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use('/api', api); // Uses api route for REST services
 // ============= HEROKU SETUP =================
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'enomerate-react/build')));
+  app.use(express.static(path.join(__dirname, '/enomerate-react/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'enomerate-react/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/enomerate-react/build', 'index.html'));
   });
 }
 
