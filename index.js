91a97982a2fab2095dafe0d7f7fa4ede059c2067
unknown <dev.to.quang.n@gmail.com>
2021-06-06 15:34:42 -0700

    point server to frontend build directory

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.listen( PORT,function(){
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'enomerate-react')));
+  app.use(express.static(path.join(__dirname, 'enomerate-react/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'enomerate-react/public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'enomerate-react/build', 'index.html'));
   });
 }
 
